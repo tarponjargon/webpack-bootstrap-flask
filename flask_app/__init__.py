93bbe86c6df3cafa2ff2bc9f7e1e800d6e4ffc87
Rory O'Connor <rory@thewhiteroom.com>
2021-07-07 22:02:16 -0700

    got site loading

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder = 'assets')
 
 @app.route("/")
 def home():
     return render_template("home.html.j2")
+
+@app.route("/about")
+def about():
+    return render_template("about.html.j2")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html.j2")
+
--- manage.py
+++ manage.py
@@ -1,16 +1,4 @@
-import sys
-import os
 from flask_app import app
-
-environment = os.getenv('ENV')
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-activate_this = dir_path + '/virtualenv_python/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
-sys.path.append(os.getcwd())
-sys.path.append(os.getcwd() + '/flask_app')
 
 if __name__ == "__main__":
     app.run()
