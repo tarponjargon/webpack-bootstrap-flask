7f5321a4e2e832577f06e67563b5075644902263
Rory O'Connor <rory@thewhiteroom.com>
2021-07-10 17:04:48 -0700

    black working

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,30 +1,35 @@
 import os
 import logging
+import pprint
 from flask import Flask, render_template
 from flask_app.pet_api import PetApi
+
 
 def page_not_found(e):
     return render_template("404.html.j2"), 404
 
-app = Flask(__name__, static_folder = 'assets')
+
+app = Flask(__name__, static_folder="assets")
 app.config.from_object("config." + "config." + os.environ["ENV"])
 app.register_error_handler(404, page_not_found)
 gunicorn_logger = logging.getLogger("gunicorn.error")
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
 
+
 @app.route("/")
 def home():
     pet_api = PetApi()
     pets = pet_api.get_pets()
-    #app.logger.warning(pets)
+    app.logger.warning(pets)
     return render_template("home.html.j2")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html.j2")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html.j2")
-
--- flask_app/pet_api.py
+++ flask_app/pet_api.py
@@ -3,68 +3,63 @@ import time
 from pprint import pprint
 from flask import current_app
 
+
 class PetApi:
     url = None
     key = None
     secret = None
-    access_token = None
-    access_token_expiration = 3500
 
     def __init__(self):
-        # the function that is executed when
-        # an instance of the class is created
         self.url = current_app.config["PETFINDER_API"]
         self.key = current_app.config["PETFINDER_API_KEY"]
         self.secret = current_app.config["PETFINDER_API_SECRET"]
-        try:
-            current_app.access_token = self.getAccessToken()
-            if current_app.access_token is None:
-                raise Exception("Request for access token failed.")
-        except Exception as e:
-            current_app.logger.error(e)
 
-
-    def getAccessToken(self):
-        # the function that is
-        # used to request the JWT
+    def request_token(self):
         try:
-            # build the JWT and store
-            # in the variable `token_body`
             token_body = {
-              "grant_type": "client_credentials",
-              "client_id": self.key,
-              "client_secret": self.secret
+                "grant_type": "client_credentials",
+                "client_id": self.key,
+                "client_secret": self.secret,
             }
-
-            # request an access token
-            request = requests.post(f"{self.url}/oauth2/token",data=token_body)
-
-            # optional: raise exception for status code
+            request = requests.post(f"{self.url}/oauth2/token", data=token_body)
             request.raise_for_status()
         except Exception as e:
             current_app.logger.error(e)
             return None
         else:
-            # assuming the response's structure is
-            # {"access_token": ""}
-            return request.json()["access_token"]
+            token = request.json()
+            token["last_auth"] = time.time()
+            current_app.config["PETFINDER_AUTH"] = token
+            print(f"TOKEN ADDED AT {token['last_auth']}")
+            return True
 
-    class Decorators():
+    class refresh_jwt:
         @staticmethod
-        def refreshToken(decorated):
-            # the function that is used to check
-            # the JWT and refresh if necessary
-            def wrapper(api,*args,**kwargs):
-                current_app.logger.error("refresh token called")
-                if not current_app.access_token or time.time() > api.access_token_expiration:
-                    current_app.access_token = api.getAccessToken()
-                    current_app.logger.error(current_app.access_token)
-                return decorated(api,*args,**kwargs)
+        def refresh_token(decorated):
+            def wrapper(api, *args, **kwargs):
+                if "PETFINDER_AUTH" not in current_app.config:
+                    print("TOKEN DOES NOT EXIST, REQUESTING")
+                    api.request_token()
+                else:
+                    print("TOKEN DOES EXIST, CHECKING EXP")
+                    pprint(current_app.config["PETFINDER_AUTH"])
+                    last_auth = current_app.config["PETFINDER_AUTH"]["last_auth"]
+                    token_exp = current_app.config["PETFINDER_AUTH"]["expires_in"]
+                    print(last_auth)
+                    print(time.time())
+                    print(token_exp)
+                    print(int(time.time() - last_auth))
+                    if int(time.time() - last_auth) > token_exp:
+                        print("TOKEN IS EXPIRED, GETTING NEW")
+                        api.request_token()
+                    else:
+                        print("TOKEN IS NOT EXPIRED")
+                return decorated(api, *args, **kwargs)
 
             return wrapper
 
-    @Decorators.refreshToken
+    @refresh_jwt.refresh_token
     def get_pets(self):
-        headers = { 'Authorization' : 'Bearer ' + current_app.access_token }
+        headers = {"Authorization": "Bearer " + current_app.config["PETFINDER_AUTH"]["access_token"]}
         response = requests.get(f"{self.url}/animals?type=dog", headers=headers)
         return response.json()
