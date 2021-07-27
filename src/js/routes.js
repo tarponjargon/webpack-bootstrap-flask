7b130fa6092ee526a525496273dec0c4dbb6d814
Rory O'Connor <rory@thewhiteroom.com>
2021-07-26 21:23:47 -0700

    gallery

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,9 +1,14 @@
-/* this is how code splitting is achieved without a front-end router.
-you add a block for each route (you can use any condition, like regex
+/* this is how code splitting is achieved when routes are handled on the server.
+You add a block for each route (you can use any condition, like regex
 or existence of a DOM element), and it will dynamically load the corresponding
 view controller JS.  the webpackPrefetch magic comments handle adding
 the "prefetch" resource hints to the DOM, so it should lazily cache the entire
-app on the first load */
+app on the first load
+
+Note I use multiple "if" rather than switch or else if, because it allows for multiple views
+to be loaded on a single page.
+
+*/
 
 const routes = async () => {
   const pathname = window.location.pathname;
@@ -23,7 +28,7 @@ const routes = async () => {
     const mod = await import(/* webpackPrefetch: true */ "./views/Puppies");
     return await new mod.default().init();
   }
-  if (pathname === "/puppy") {
+  if (pathname.startsWith("/puppy/")) {
     const mod = await import(/* webpackPrefetch: true */ "./views/Puppy");
     return await new mod.default().init();
   }
--- src/js/views/Puppy.js
+++ src/js/views/Puppy.js
@@ -1,5 +1,42 @@
-export default class Puppy {
-  constructor() {}
+import Splide from "@splidejs/splide";
 
-  init = () => {};
+export default class Puppy {
+  constructor() {
+    this.el = document.getElementById("main-image-sync");
+  }
+
+  init = () => {
+    if (this.el) this.startGallery();
+  };
+
+  startGallery = () => {
+    // Create and mount the thumbnails slider.
+    const secondarySlider = new Splide("#thumbnail-sync", {
+      rewind: true,
+      fixedWidth: 100,
+      fixedHeight: 64,
+      isNavigation: true,
+      gap: 10,
+      pagination: false,
+      cover: true,
+      breakpoints: {
+        600: {
+          fixedWidth: 66,
+          fixedHeight: 40,
+        },
+      },
+    }).mount();
+
+    // Create the main slider.
+    const primarySlider = new Splide("#main-image-sync", {
+      type: "fade",
+      heightRatio: 0.9,
+      pagination: false,
+      arrows: false,
+      cover: true,
+    });
+
+    // Set the thumbnails slider as a sync target and then call mount.
+    primarySlider.sync(secondarySlider).mount();
+  };
 }
