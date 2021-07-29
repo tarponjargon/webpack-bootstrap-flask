1f092081472b2574177b04c68a836a34285624e9
Rory O'Connor <rory@thewhiteroom.com>
2021-07-28 19:50:47 -0700

    async modal puppies

--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,6 +1,7 @@
 import { Modal } from "bootstrap";
 import Splide from "@splidejs/splide";
 import { getJson } from "../modules/Utils";
+import { renderSubfeature } from "../components/Subfeature";
 import { renderModal } from "../components/Modal";
 
 export default class Home {
@@ -8,14 +9,13 @@ export default class Home {
     this.modal = new Modal(document.getElementById("myModal"));
     this.subfeaturesEl = document.querySelector('[data-js="subfeatures-container"]');
     this.modalContentEl = document.querySelector('[data-js="modal-content"]');
-
     this.carouselEl = document.querySelector("[data-js='homepage-carousel']");
-    this.slider = null; // carousel instance
+    this.subFeatures = [];
   }
 
   init = () => {
     return new Promise((resolve) => {
-      this.slider = new Splide(this.carouselEl, {
+      const slider = new Splide(this.carouselEl, {
         perPage: 3,
         perMove: 3,
         rewind: true,
@@ -32,18 +32,36 @@ export default class Home {
           },
         },
       });
-      this.slider.mount();
-      const modals = Array.from(document.querySelectorAll("[data-puppy-modal]"));
-      modals.map((s) => s.addEventListener("click", this.modalHandler, false));
-
-      resolve();
+      slider.mount();
+      this.loadSubfeatures().then(() => resolve());
     });
   };
+
+  loadSubfeatures = async () => {
+    const data = await getJson(`/api/puppies`);
+    this.puppies = data.animals
+      .filter((i) => i.primary_photo_cropped && i.primary_photo_cropped.medium)
+      .slice(0, 6);
+    this.renderSubfeatures(this.puppies);
+    const modals = Array.from(this.subfeaturesEl.querySelectorAll("[data-puppy-modal]"));
+    modals.map((s) => s.addEventListener("click", this.modalHandler, false));
+  };
+
+  renderSubfeatures = (puppies) => {
+    const html = `
+      <div class="row justify-content-center">
+        ${puppies.map((p) => renderSubfeature(p)).join("")}
+      </div>
+    `;
+
+    this.subfeaturesEl.innerHTML = html;
+  };
+
   modalHandler = async (e) => {
     e.preventDefault();
     if (e.currentTarget.getAttribute("data-puppy-modal")) {
       const id = e.currentTarget.getAttribute("data-puppy-modal");
-      const puppy = await getJson(`/api/puppy/${id}`);
+      const puppy = this.puppies.find((i) => i.id.toString() === id.toString());
       this.modalContentEl.innerHTML = renderModal(puppy);
       this.modal.show();
     }
