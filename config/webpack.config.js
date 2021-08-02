29c5f0df38012145fb4a4e08f9460e3cc7535e4c
Rory O'Connor <rory@thewhiteroom.com>
2021-08-02 10:41:03 -0700

    bundle analyzer

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,6 +5,7 @@ const path = require("path");
 const config = require("config");
 const glob = require("glob-all");
 
+//const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const PurgecssPlugin = require("purgecss-webpack-plugin");
@@ -215,6 +216,7 @@ module.exports = (env, argv) => {
       new CopyWebpackPlugin({
         patterns: [{ from: "src/images", to: images }],
       }),
+      //new BundleAnalyzerPlugin(),
     ]),
   };
 };
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,9 +1,15 @@
-/* this is how code splitting is achieved when routes are handled on the server.
+/*
+
+this is how code splitting is achieved when routes are handled on the server.
 You add a block for each route (tho you can use any condition, like
 existence of a DOM element), and it will dynamically load the corresponding
 module.  the webpackPrefetch magic comments handle adding
 the "prefetch" resource hints to the DOM, so it should lazily cache the entire
 app on the first load
+
+webpackChunkName magic comments are not required, I use them so I can easily see and
+understand the chunk sizes in the outputted files.  Uncommenting BundleAnalyzerPlugin lines
+in the webpack config and doing 'npm run build' is also a good way to visualize
 
 Note I use multiple "if" rather than switch or else if, because it allows for multiple views
 to be loaded on a single page.
@@ -13,27 +19,51 @@ to be loaded on a single page.
 const routes = async () => {
   const pathname = window.location.pathname;
   if (pathname === "/") {
-    const mod = await import(/* webpackPrefetch: true */ "./views/Home");
+    const mod = await import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: "home" */
+      "./views/Home"
+    );
     return await new mod.default().init();
   }
   if (pathname === "/about") {
-    const mod = await import(/* webpackPrefetch: true */ "./views/About");
+    const mod = await import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: "about" */
+      "./views/About"
+    );
     return await new mod.default().init();
   }
   if (pathname === "/contact") {
-    const mod = await import(/* webpackPrefetch: true */ "./views/Contact");
+    const mod = await import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: "contact" */
+      "./views/Contact"
+    );
     return await new mod.default().init();
   }
   if (pathname === "/puppies") {
-    const mod = await import(/* webpackPrefetch: true */ "./views/Puppies");
+    const mod = await import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: "puppies" */
+      "./views/Puppies"
+    );
     return await new mod.default().init();
   }
   if (pathname.startsWith("/puppy/")) {
-    const mod = await import(/* webpackPrefetch: true */ "./views/Puppy");
+    const mod = await import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: "puppy" */
+      "./views/Puppy"
+    );
     return await new mod.default().init();
   }
   if (pathname === "/favorites") {
-    const mod = await import(/* webpackPrefetch: true */ "./views/FavoritesView");
+    const mod = await import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: "favoritesview" */
+      "./views/FavoritesView"
+    );
     return await new mod.default().init();
   }
 };
