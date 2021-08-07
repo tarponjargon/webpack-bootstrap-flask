f8f2f1e1c70dd4eecfa3dd3eb0ba42487541b47f
Rory O'Connor <rory@thewhiteroom.com>
2021-08-06 20:03:57 -0700

    gunicorn config

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -3,11 +3,12 @@
 import os
 from glob import glob
 
-workers = 1
-threads = 3
-bind = "localhost:" + os.environ["FLASK_PORT"] or 5000
+workers = os.environ.get("GUNICORN_WORKERS") or 1
+threads = os.environ.get("GUNICORN_THREADS") or 3
+bind = "localhost:" + os.environ.get("FLASK_PORT") or 5000
 reload = True
 reload_extra_files = glob("flask_app/templates/**/*", recursive=True)
 errorlog = "logs/gunicorn-error.log"
 accesslog = "logs/gunicorn-access.log"
+pidfile = "tmp/gunicorn.pid"
 loglevel = "debug"
